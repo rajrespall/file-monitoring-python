437a92171e4aadcaf93efc05a12c5ebad4a66db7
RajTheGreatest <rajesh.respall@tup.edu.ph>
2024-11-15 12:19:01 +0800

    fix baseline, config, scan

--- backend/baseline/serializers.py
+++ backend/baseline/serializers.py
@@ -4,12 +4,12 @@ from .models import Baseline, Config, Scan
 class BaselineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Baseline
-        fields = ['id', 'user', 'path', 'hash_value', 'algorithm']
+        fields = ['id', 'user','original_filename', 'path', 'hash_value', 'algorithm']
         
 class ConfigSerializer(serializers.ModelSerializer):
     class Meta:
         model = Config
-        fields = ['user', 'algorithm', 'scan_frequency']
+        fields = ['user', 'path', 'algorithm', 'scan_frequency']
         
     def validate_algorithm(self,value):
         allowed_algorithms = ['md5','sha256']
@@ -23,8 +23,15 @@ class ConfigSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(f"Frequency {value} is not allowed")
         return value
     
+class ScanResultSerializer(serializers.Serializer):
+    filename = serializers.CharField()
+    status = serializers.CharField()
+    expected_hash = serializers.CharField()
+    current_hash = serializers.CharField(allow_null=True)
+
 class ScanSerializer(serializers.ModelSerializer):
+    results = ScanResultSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Scan
-        fields = '__all__'
-    
+        fields = ['id', 'status', 'created_at', 'results']
